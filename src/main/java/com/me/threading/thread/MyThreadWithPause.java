571ca953c2f3f4dfc20ed81d20654adfc684d97b
marcelofourquet <marcelofourquet@gmail.com>
2019-05-06 08:04:43 -0300

    MyThreadWithPause has changed

--- src/main/java/com/me/threading/thread/MyThreadWithPause.java
+++ src/main/java/com/me/threading/thread/MyThreadWithPause.java
@@ -1,8 +1,6 @@
 
 package com.me.threading.thread;
 
-import static org.apache.commons.lang3.RandomUtils.nextInt;
-
 class MyThreadWithPause extends Thread{
 
 	private int time;
@@ -18,9 +16,8 @@ class MyThreadWithPause extends Thread{
 	public void run(){
 		for(int i = 1; i <= 20; i++){
 			System.out.printf("Running %s - Step: %d%n", name, i);
-			time = nextInt(1000, 2000);
-			System.out.printf("%s paused %d miliseconds%n", name, time);
 			try{
+				System.out.printf("%s paused %d miliseconds%n", name, time);
 				Thread.sleep(time);
 			}catch(InterruptedException ex){
 				System.out.println(ex.getMessage());
--- src/main/java/com/me/threading/thread/RunnerThreadWithPause.java
+++ src/main/java/com/me/threading/thread/RunnerThreadWithPause.java
@@ -1,11 +1,16 @@
 
 package com.me.threading.thread;
 
+import java.util.Random;
+
 public class RunnerThreadWithPause{
 
 	public static void main(String[] args){
+		Random rnd = new Random();
+		int time;
 		for(int i = 1; i <= 3; i++){
-			MyThreadWithPause myThread = new MyThreadWithPause(0, "Thread " + i);
+			time = rnd.nextInt(1000) + 1000;
+			MyThreadWithPause myThread = new MyThreadWithPause(time, "Thread " + i);
 			myThread.start();
 		}
 	}
